3139b2380d5c89cedb3f6ed99fe30da2a7257742
Jarmila Havířová <161177140+jarmilahavirova@users.noreply.github.com>
2024-05-21 19:21:26 +0200

    Zprovoznění výběru data cesty z API

--- src/components/JourneyPicker/index.jsx
+++ src/components/JourneyPicker/index.jsx
@@ -16,23 +16,47 @@ const CityOptions = ({ cities }) => {
   );
 };
 
+const DatesOptions = ({ dates }) => {
+  return (
+    <>
+      <option value="">Vyberte</option>
+      {dates.map((date) => {
+        return (
+          <option key={date.dateBasic} value={date.dateBasic}>
+            {date.dateCs}
+          </option>
+        );
+      })}
+    </>
+  );
+};
+
 export const JourneyPicker = ({ onJourneyChange }) => {
   const [fromCity, setFromCity] = useState("");
   const [toCity, setToCity] = useState("");
   const [date, setDate] = useState("");
   const [cities, setCities] = useState([]);
+  const [dates, setDates] = useState([]);
 
   useEffect(() => {
-    const fetchData = async () => {
+    const fetchCities = async () => {
       const response = await fetch(
         "https://apps.kodim.cz/daweb/leviexpress/api/cities"
       );
       const json = await response.json();
-
       setCities(json.results);
     };
 
-    fetchData();
+    const fetchDates = async () => {
+      const response = await fetch(
+        "https://apps.kodim.cz/daweb/leviexpress/api/dates"
+      );
+      const json = await response.json();
+      setDates(json.results);
+    };
+
+    fetchCities();
+    fetchDates();
   }, []);
 
   const handleSubmit = (event) => {
@@ -76,12 +100,7 @@ export const JourneyPicker = ({ onJourneyChange }) => {
                 setDate(event.target.value);
               }}
             >
-              <option value="">Vyberte</option>
-              <option value="datum01">Datum 01</option>
-              <option value="datum02">Datum 02</option>
-              <option value="datum03">Datum 03</option>
-              <option value="datum04">Datum 04</option>
-              <option value="datum05">Datum 05</option>
+              <DatesOptions dates={dates} />
             </select>
           </label>
           <div className="journey-picker__controls">
