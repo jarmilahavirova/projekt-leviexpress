adb999369669e05b18f71681b1f46067e67687ec
Jarmila Havířová <161177140+jarmilahavirova@users.noreply.github.com>
2024-05-21 17:27:54 +0200

    Naroutování stránky, vytvoření Reservation Page

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,28 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { createBrowserRouter, RouterProvider, Link } from 'react-router-dom';
-import { App } from './components/App';
-import './global.css';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider, Link } from "react-router-dom";
+import { App } from "./components/App";
+import "./global.css";
+import { HomePage } from "./pages/HomePage";
+import { ReservationPage } from "./pages/ReservationPage";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+      {
+        path: "/reservation",
+        element: <ReservationPage />,
+      },
+    ],
   },
 ]);
 
-createRoot(document.querySelector('#app')).render(
+createRoot(document.querySelector("#app")).render(
   <RouterProvider router={router} />
 );
