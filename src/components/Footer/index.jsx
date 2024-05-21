1928bd67f4cb652c90553934bfd691f03788e45f
Jarmila Havířová <161177140+jarmilahavirova@users.noreply.github.com>
2024-05-21 17:20:04 +0200

    Opraven rok

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,14 +1,12 @@
-import React from 'react';
-import './style.css';
+import React from "react";
+import "./style.css";
 
 export const Footer = () => (
   <footer>
     <div className="container">
-      <div className="footer-title">
-        LeviExpress
-      </div>
+      <div className="footer-title">LeviExpress</div>
       <div className="copyright">
-        Czechitas Digitální akademie: Web, © 2021 - 2023
+        Czechitas Digitální akademie: Web, © 2021 - 2024
       </div>
     </div>
   </footer>
